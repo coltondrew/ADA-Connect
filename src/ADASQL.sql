93d63926b27341b66898d88f6515417e14df3d56
coltondrew <659colton@gmail.com>
2019-03-04 18:42:58 -0600

    Added AddUser Function

--- src/ADASQL.sql
+++ src/ADASQL.sql
@@ -8,55 +8,69 @@ create table Admins(
     password varchar(40),
 	firstname varchar(40),
     lastname varchar(40),
-	role varchar(20)
+	role varchar(20),
+    primary key(username)
 );
 
+create table Teams(
+	teamname varchar(40),
+    teamID int auto_increment,
+    username varchar(20),
+    latitude int(10),
+    longitude int(10),
+    primary key (teamid),
+    foreign key (username) references Admins(username)
+);
 
+create table Volunteers(
+	firstname varchar(40),
+    lastname varchar(40),
+	volID int auto_increment,
+    schoolyear int,
+    hometown varchar(40),
+    highschool varchar(40),
+    bio varchar(300),
+    team varchar(40),
+    pictureurl varchar(100),
+    startyear int,
+    active tinyint,
+    primary key(volID)
+);
 
--- Example Admin Insert
+create table Stats(
+	volID int auto_increment,
+    teamID int,
+    conversations int,
+    conversions int,
+    numyear int,
+    numweek int,
+    primary key(userid, teamid, numyear, numweek),
+    foreign key(userid) references Volunteers(userid),
+    foreign key(teamid) references Teams(teamid)
+);
+
+create table Applications(
+	answer1 varchar(40),
+	answer2 varchar(40),
+	complete tinyint,
+	accepted tinyint
+);
+
+create table News(
+	title varchar(100),
+	contents varchar(500),
+	datemade datetime,
+	pictureurl varchar(100)
+);
+
+-- Example Inserts
 insert into Admins(username, password, firstname, lastname, role)
-	values('testuser', sha1('testpass'), 'First', 'Last', 'Test', 'Test team');
+	values('testuser', sha1('testpass'), 'First', 'Last', 'Test');
+    
+
 
 select username from Admins where username='testuser' and password=sha1('testpass');
 select * from Admins;
 
 -- Delete table if needed
 drop table Admins;
-
-create table Teams(
-	teamname varchar(40),
-    teamID varchar(20),
-    username varchar(20),
-    latitude int(10),
-    longitude int(10)
-);
-
-create table Volunteers(
-	firstname varchar(40),
-    lastname varchar(40),
-    schoolyear varchar(40),
-    hometown varchar(40),
-    highschool varchar(40),
-    bio varchar(40),
-    team varchar(40),
-    picture varchar(40),
-    volID varchar(40),
-    startyear varchar(40),
-    active tinyint
-);
-
-create table Stats{
-	userid
-    teamid
-    conversations
-    conversions
-    year
-    week
-};
-
--- Team Leaders
-create table Applications{
-answer1
-answer2
-...
-};
--- src/mySql/MySqlConnections.java
+++ src/mySql/MySqlConnections.java
@@ -17,6 +17,7 @@ public class MySqlConnections {
 	   public static User Login(String username, String password) {
 		   connection = null;
 		   User user = null;
+		   PreparedStatement statement = null;
 		      try {
 		         connection = DriverManager.getConnection(url, sqluser, sqlpassword);
 		      } catch (SQLException e) {
@@ -29,35 +30,81 @@ public class MySqlConnections {
 		      }
 		      try {
 					String loginSQL = "select firstname, lastname, role from Admins where username =? and password =sha1(?)";
-					PreparedStatement preparedStatement = connection.prepareStatement(loginSQL);
-					preparedStatement.setString(1, username);
-					preparedStatement.setString(2, password);
-					ResultSet rs = preparedStatement.executeQuery();
+					statement = connection.prepareStatement(loginSQL);
+					statement.setString(1, username);
+					statement.setString(2, password);
+					ResultSet rs = statement.executeQuery();
 					if( rs.next()) {
 						user = new User(username,
 								rs.getString(1),
 								rs.getString(2),
 								rs.getString(3));
-
+						statement.close();
+						connection.close();
 					}
-					preparedStatement.close();
-					connection.close();
+
 		      } catch (SQLException e) {
 		         e.printStackTrace();
 		      }
-
 		   return user;
 	   }
 	   
+	   public static boolean AddUser(String username, String password, String firstname, String lastname, String role) {
+		   connection = null;
+		   boolean complete = false;
+		   PreparedStatement statement = null;
+			String addUser = "insert into Admins(username, password, firstname, lastname, role) " + 
+					"values(?,?,?,?,?)";
+		      try {
+		         connection = DriverManager.getConnection(url, sqluser, sqlpassword);
+		      } catch (SQLException e) {
+		         System.out.println("Connection Failed! Check output console");
+		         e.printStackTrace();
+		      }
+		      if (connection == null) {
+		         System.out.println("Failed to make connection!");
+		         return complete;
+		      }
+		      try {
+					statement = connection.prepareStatement(addUser);
+					statement.setString(1, username);
+					statement.setString(2, password);
+					statement.setString(3, firstname);
+					statement.setString(4, lastname);
+					statement.setString(5, role);
+					if(statement.executeUpdate() > 0) {
+						complete = true;
+					}
+					statement.close();
+					connection.close();
+
+		      } catch (SQLException e) {
+		         e.printStackTrace();
+		      }
+		   return complete;
+	   }
+	   
+	   
+	   
 	   public static void main(String[] args) {
 		   //Test functions
-		   User user = Login("testuser", "testpass");
+		   
+		   // Login Test
+		   /*User user = Login("testuser", "testpass");
 		   if(user != null) {
 			   System.out.println("Test user logged in");
 			   System.out.println(user);
 		   }
 		   else {
 			   System.out.println("Test user failed to log in");
+		   }*/
+		   
+		   // Insert User Test, will fail if user already exists
+		   if(AddUser("testuser2", "password", "User", "Test", "Role")) {
+			   System.out.println("Add user successful");
+		   }
+		   else {
+			   System.out.println("Add test user failed");
 		   }
 	   }
 }
